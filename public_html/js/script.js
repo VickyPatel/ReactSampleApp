97cd40bb6740d7effb0293a6b748dce2c80a8a01
Vicky <vicky.7676@yahoo.in>
2016-06-03 12:55:33 -0400

    first commit

---
+++ public_html/js/script.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+  render(){
+    return (
+   		<div>
+   			<h1>Hello world. change. update awesome</h1>
+      	</div>
+    );
+  }
+};
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout/>, app);
